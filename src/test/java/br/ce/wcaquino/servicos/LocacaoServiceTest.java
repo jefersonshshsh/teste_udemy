b1b38f5317ad9046af902dd526555e32088704b6
jefersonshshsh <jeferson.shshsh@gmail.com>
2019-10-29 15:44:15 -0300

    Finalizacao aula 9

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -17,6 +17,7 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -28,6 +29,9 @@ public class LocacaoServiceTest {
 	
 	@Rule
 	public ErrorCollector error = new ErrorCollector();
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 
 	@Test
 	public void teste() throws Exception {	
@@ -77,8 +81,23 @@ public class LocacaoServiceTest {
 		//acao
 		try {
 			Locacao locacao = service.alugarFilme(usuario, filme);
+			Assert.fail("Deveria ter lancado uma excecao");
 		} catch (Exception e) {
 			Assert.assertThat(e.getMessage(), is("Filme sem estoque"));
 		}
+	}
+	
+	@Test
+	public void testLocacao_filmeSemEstoque3() throws Exception{
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+
+		exception.expect(Exception.class);
+		exception.expectMessage("Filme sem estoque");
+		
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
 	}
 }
