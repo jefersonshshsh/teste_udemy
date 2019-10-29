025d75970dacd38e8e7c41cb9dd53a57815d8ea8
jefersonshshsh <jeferson.shshsh@gmail.com>
2019-10-29 18:51:24 -0300

    aula 12

---
+++ src/test/java/br/ce/wcaquino/servicos/OrdemTest.java
@@ -0,0 +1,38 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class OrdemTest {
+	
+	public static int contador = 0;
+
+	@Test
+	public void inicia(){
+		contador = 1;
+	}
+	
+	@Test
+	public void verfica(){
+		Assert.assertEquals(1, contador);
+	}
+	
+	@Test
+	public void verfica1(){
+		
+	}
+	
+	@Test
+	public void verfica2(){
+		
+	}
+	
+	@Test
+	public void verfica3(){
+		
+	}
+}
